244ba8f8233bbf75575c1936f94e64ce3cc57442
root <root@ip-172-31-33-226.ap-south-1.compute.internal>
2026-06-20 16:02:29 +0000

    Addedd new code

--- app.py
+++ app.py
@@ -4,7 +4,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Hello from Flask App running on EC2 + Docker!"
+    return """
+    <h1>🚀 DevOps CI/CD Pipeline</h1>
+    <p>Application deployed successfully!</p>
+
+    <h3>Project Details</h3>
+    <ul>
+        <li>✅ GitHub Actions CI/CD</li>
+        <li>✅ Docker</li>
+        <li>✅ Docker Hub</li>
+        <li>✅ AWS EC2</li>
+        <li>✅ Flask Application</li>
+    </ul>
+
+    <p><b>Author:</b> Manmeet Kaur</p>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
